da84400c8f2dcd4e4dd25ce2bda1da6ce00a282b
Tang Eason <tangyisheng2@sina.com>
2021-08-05 15:07:12 +0800

    Custom hook for Home Page

---
+++ 0.myapp/react-rmdb/src/components/Home.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+// API
+import API from "../API";
+// Config
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
+// Components
+
+// Hook
+import { useHomeFetch } from "../hook/usdHomeFetch";
+// Image
+import NoImage from "../images/no_image.jpg";
+
+const Home = () => {
+  // const [state, setState] = useState();
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(false);
+
+  // const fetchMovies = async (page, searchTerm = "") => {
+  //   try {
+  //     setError(false);
+  //     setLoading(true);
+
+  //     const movies = await API.fetchMovies(searchTerm, page);
+  //     console.log(movies);
+
+  //     setState((prev) => ({
+  //       ...movies,
+  //       results:
+  //         page > 1 ? [...prev.results, ...movies.results] : [...movies.results],
+  //     }));
+  //   } catch (error) {
+  //     setError(true);
+  //   }
+  //   setLoading(false);
+  // };
+  // // Initial render
+  // useEffect(() => {
+  //   fetchMovies(1);
+  // }, []);
+  // Move to hook/useHomeFetch.js
+
+  const { state, loading, error } = useHomeFetch()
+  
+  console.log(state);
+  return <div>Home Page</div>;
+};
+
+export default Home;
